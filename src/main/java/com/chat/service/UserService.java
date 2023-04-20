4cb3ad35da0d8682ac4200efab9e0bc0a78f205d
Lenny <2029194345@qq.com>
2023-04-20 21:38:20 +0800

    登录、注册前端、后端

--- src/main/java/com/chat/service/UserService.java
+++ src/main/java/com/chat/service/UserService.java
@@ -1,6 +1,8 @@
 package com.chat.service;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.ObjectUtils;
 import com.chat.dao.UserDao;
 import com.chat.entity.User;
 import com.chat.utils.Email;
@@ -9,8 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Service//表示是服务层
 public class UserService {
@@ -20,17 +24,28 @@ public class UserService {
     RedisTemplate redisTemplate;
 
     @Autowired
-    Email email;
+    Email Email;
 
     //发送邮箱方法
-    public Result sendCode(String userEmail){
+    public Result sendCode(String email){
+        //判断邮箱是否注册过
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("email",email);
+        User user = dao.selectOne(wrapper);
+        if(!ObjectUtils.isNull(user)){
+            return Result.error("该邮箱已经注册!");
+        }
+
         //生成验证码
         String code="";
         for (int i=1;i<=6;i++){
             code+=new Random().nextInt(10);
         }
         //发送验证码
-        boolean result = email.sendEmail(userEmail, "验证码", "你的验证码是:" + code);
+        boolean result = Email.sendEmail(email, "验证码", "你的验证码是:" + code);
+
+        //将验证码放入redis中存储30秒
+        redisTemplate.opsForValue().set("code",code,30, TimeUnit.SECONDS);
         return result?Result.ok("发送成功!"):Result.error("发送失败!");
 
     }
@@ -46,5 +61,24 @@ public class UserService {
         //2、验证码正确   进行注册
         dao.insert(user);
         return Result.ok("注册成功!");
+    }
+
+    //登录
+    public Result login(User user){
+        //判断邮箱是否已经注册
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("email",user.getEmail());//(数据库字段,实体类字段)
+        User u = dao.selectOne(wrapper);
+        if(ObjectUtils.isNull(u)){
+            return Result.error("该邮箱未注册!");
+        }
+
+        //执行登录
+        wrapper.eq("password",user.getPassword());
+        u = dao.selectOne(wrapper);
+        if(ObjectUtils.isNull(u)){
+            return Result.error("密码输入错误!");
+        }
+        return Result.ok("登录成功!");
     }
 }
